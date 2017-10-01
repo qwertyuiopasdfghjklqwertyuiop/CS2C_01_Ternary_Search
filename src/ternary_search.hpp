b9dd16365dc48e177cc6952d63e7599b3605415d
qwertyuiopasdfghjklqwertyuiop <bobthefrank24685@gmail.com>
2017-10-01 12:20:10 -0700

    increased readability of ternary_search()

--- src/ternary_search.hpp
+++ src/ternary_search.hpp
@@ -6,19 +6,20 @@ int ternary_search(const ArrayLike & array, const T& value, int low, int high)
    if(high <= low) return -1;
 
    int third = (high - low) / 3;
-   int location = low + third;
+   int location1 = low + third;
+   int location2 = low + third + third;
 
-   if( value == array[location] )
-      return location;
-   else if( value < array[location] )
-      return ternary_search( array, value, low, location );
+   if( value == array[location1] )
+      return location1;
+   else if( value < array[location1] )
+      return ternary_search( array, value, low, location1 );
    
-   if( value == array[location + third] )
-      return location + third;
-   else if( value < array[location + third] )
-      return ternary_search( array, value, location, location + third );
-   else if( value > array[location + third] )
-      return ternary_search( array, value, location + third + 1, high );
+   if( value == array[location2] )
+      return location2;
+   else if( value < array[location2] )
+      return ternary_search( array, value, location1, location2 );
+   else if( value > array[location2] )
+      return ternary_search( array, value, location2 + 1, high );
 
    else return -1;
 }
